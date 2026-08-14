851bc74251bef184a32e005cc1acbcdd53e00eea
Jenna Kalina <jenna.kali28@gmail.com>
2026-08-14 16:51:59 -0400

    Cleaned up code

--- scripts/03_monocle3.R
+++ scripts/03_monocle3.R
@@ -1,14 +1,13 @@
 library(monocle3)
 library(dplyr)
 library(patchwork)
-setwd("~/Documents/scRNA/with/Sudhir/leukemia_reanalysis")
 
 # Read in
 mat <- read.delim('data/matrix_GEO.txt', check.names = FALSE, row.names = 1)
 meta <- read.delim('data/metadata_GEO.txt')
 gene_ann <- read.csv('data/gene_annotations.csv', row.names = 1)
 
-## SPLIT INTO GENOTYPE
+# Split by genotype
 meta <- meta %>% filter(!barcode == '')
 rownames(meta) <- meta$barcode
 
@@ -28,20 +27,17 @@ cds <- new_cell_data_set(mat, cell_metadata = meta, gene_metadata = gene_ann)
 cds <- preprocess_cds(cds, num_dim = 100)
 
 cds <- reduce_dimension(cds, preprocess_method = 'PCA')
-#cds@int_colData@listData[["reducedDims"]]@listData[["PCA"]] <- 
-#  PC_DB_DT@reductions[["pca"]]@cell.embeddings     # To use old PCA and UMAP, which I don't have
-#cds@int_colData@listData[["reducedDims"]]@listData[["UMAP"]] <- PC_DB_DT@reductions[["umap"]]@cell.embeddings
 colData(cds)@listData[["Celltype"]] <- meta$cluster
 cds@clusters@listData[["UMAP"]][["clusters"]] <- meta$cluster
 cds@clusters@listData[["UMAP"]][["partitions"]] <- meta$cluster
 
 # Check UMAP
-#genes_check <- c('hop', 'EcR', 'foxo', 'CalpB', 'gal4', 'Mrtf')
+# genes_check <- c('hop', 'EcR', 'foxo', 'CalpB', 'gal4', 'Mrtf')
 genes_check <- c('NimC1', 'eater', 'lz', 'PPO1', 'atilla', 'PPO3')
 
 plot_cells(cds, genes = genes_check, label_groups_by_cluster=FALSE,  color_cells_by = "Celltype", show_trajectory_graph = FALSE)
 
-# run monocle3
+# Run monocle3
 cds <- cluster_cells(cds)
 cds <- learn_graph(cds,use_partition = F)
 
@@ -74,7 +70,7 @@ plot_cells(cds, color_cells_by = "pseudotime", label_cell_groups=FALSE,
            label_leaves=FALSE, label_branch_points=FALSE, group_label_size=3.5)
 dev.off()
 
-png('results/trajectoryAnalysis/emptyCtrl_trajectory_WITHLEGEND.png', width = 12, height = 6, units = 'in', res = 300)
+png('results/trajectoryAnalysis/emptyCtrl_trajectory_with_legend.png', width = 12, height = 6, units = 'in', res = 300)
 plot_cells(cds, color_cells_by = "Celltype", label_cell_groups=FALSE, group_cells_by = 'Celltype',
              label_leaves=FALSE, label_branch_points=FALSE, group_label_size=3.5)
 dev.off()
@@ -103,16 +99,6 @@ pseudotime_table <- pseudotime_table[rownames(meta)] %>% data.frame()
 #            label_leaves=TRUE,
 #            label_branch_points=FALSE,
 #            graph_label_size=2.5)
-
-# Below is just copied from trachea paper, maybe look into it later
-# identify genes differentially regulated in pseudotime
-pr_test_res <- graph_test(cds, neighbor_graph="principal_graph")
-pr_deg_ids <- row.names(subset(pr_test_res, q_value < 0.05))
-gene_module_df <- find_gene_modules(cds[pr_deg_ids,], resolution=0.001)
-names(gene_module_df)[1] <- "gene_short_name"
-pr_merge <- merge(pr_test_res, gene_module_df, by = "gene_short_name")
-
-
 
 
 
